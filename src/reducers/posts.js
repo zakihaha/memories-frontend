c314ba98f0976abe9b58c1368b46b4e90e07ba16
Muh Zaki Choiruddin <zakichoiruddin24@gmail.com>
2022-05-25 23:02:41 +0700

    create constant action type

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,16 +1,18 @@
+import { CREATE, DELETE, FETCH_ALL, LIKE, UPDATE } from '../constants/actionTypes';
+
 // reducer membutuhkan state dan action
 export default (posts = [], action) => {
     switch (action.type) {
         // membuat state baru
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload
-        case 'CREATE':
+        case CREATE:
             return [...posts, action.payload]
-        case 'UPDATE':
-        case 'LIKE':
+        case UPDATE:
+        case LIKE:
                                                     // id updated post
             return posts.map((post) => post._id === action.payload._id ? action.payload : post)
-        case 'DELETE':
+        case DELETE:
             return posts.filter((post) => post._id !== action.payload)
         default:
             return posts
