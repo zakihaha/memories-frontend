78f92c145c0092b33c8f009d8b1d3e9f204044ec
Muh Zaki Choiruddin <zakichoiruddin24@gmail.com>
2022-05-25 22:06:11 +0700

    delete post

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -9,6 +9,8 @@ export default (posts = [], action) => {
         case 'UPDATE':
                                                     // id updated post
             return posts.map((post) => post._id === action.payload._id ? action.payload : post)
+        case 'DELETE':
+            return posts.filter((post) => post._id !== action.payload)
         default:
             return posts
     }
