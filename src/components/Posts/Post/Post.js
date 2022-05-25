2143f5c9e5e0333a35d22c43358f60bc6026afa5
Muh Zaki Choiruddin <zakichoiruddin24@gmail.com>
2022-05-25 22:35:46 +0700

    like post

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -6,7 +6,7 @@ import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
-import { deletePost } from '../../../actions/posts';
+import { deletePost, likePost } from '../../../actions/posts';
 
 function Post({ post, setCurrentId }) {
     const classes = useStyles()
@@ -32,7 +32,7 @@ function Post({ post, setCurrentId }) {
                 <Typography variant='h5' gutterBottom> {post.message} </Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size='small' color='primary' onClick={() => { }}>
+                <Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize='small' />
                     Like
                     {post.likeCount}
