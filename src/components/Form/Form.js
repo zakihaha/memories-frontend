6e8ed6d6b6dad51f21f05381b0cc560bbd7f6053
Muh Zaki Choiruddin <zakichoiruddin24@gmail.com>
2022-05-29 13:43:53 +0700

    assign user to post and improve post like logic

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -7,9 +7,11 @@ import { createPost, updatePost } from '../../actions/posts';
 import useStyles from './styles'
 
 function Form({ currentId, setCurrentId }) {
+    const classes = useStyles()
+    const dispatch = useDispatch()
+
     // use state gabungan satu form
     const [postData, setPostData] = useState({
-        creator: '',
         title: '',
         message: '',
         tags: '',
@@ -18,37 +20,44 @@ function Form({ currentId, setCurrentId }) {
 
     const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null)
 
-    const classes = useStyles()
-    const dispatch = useDispatch()
+    const user = JSON.parse(localStorage.getItem('profile'))
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
         if (currentId) {
-            dispatch(updatePost(currentId, postData))
+            dispatch(updatePost(currentId, { ...postData, name: user?.result?.name }))
         } else {
-            dispatch(createPost(postData))
+            dispatch(createPost({ ...postData, name: user?.result?.name }))
         }
-
         clear()
     }
 
     const clear = () => {
         setCurrentId(null)
-        setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '', })
+        setPostData({ title: '', message: '', tags: '', selectedFile: '', })
     }
 
     useEffect(() => {
-        if(post) setPostData(post)
+        if (post) setPostData(post)
         // ketika ada yg berganti, run
     }, [post])
+
+    if (!user?.result?.name) {
+        return (
+            <Paper className={classes.paper}>
+                <Typography variant='h6' align='center'>
+                    Please Sign In to create your own memories and like other's memories
+                </Typography>
+            </Paper>
+        )
+    }
 
     return (
         <Paper className={classes.paper}>
             <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant='h6'>{ currentId ? "Editing" : "Creating" } a memory</Typography>
+                <Typography variant='h6'>{currentId ? "Editing" : "Creating"} a memory</Typography>
                 {/* agar tidak hilang */}
-                <TextField name='creator' variant='outlined' label='Creator' fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
                 <TextField name='title' variant='outlined' label='Title' fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
                 <TextField name='message' variant='outlined' label='Message' fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
                 <TextField name='tags' variant='outlined' label='Tags' fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
